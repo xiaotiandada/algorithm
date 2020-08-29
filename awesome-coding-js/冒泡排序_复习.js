e82bcdeaee2d6c38f8ebeca0eddb246843c4a217
xiaotiandada <istianlei@qq.com>
2020-08-29 23:12:03 +0800

    冒泡复习

---
+++ awesome-coding-js/冒泡排序_复习.js
@@ -0,0 +1,27 @@
+let lists = []
+for(let i = 0; i <10; i++) {
+    lists.splice(Math.floor(Math.random() * lists.length + 1), 0, i)
+}
+
+console.log('lists', lists)
+
+function bubbleSort(array) {
+
+    let len = array.length
+    for (let i = 0; i < len; i++) {
+        let complete = true
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+                complete = false
+            }
+        }        
+        if (complete) {
+            break
+        }
+    }
+
+    return array
+}
+
+console.log('lists', bubbleSort(lists))
